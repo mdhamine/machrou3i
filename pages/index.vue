deafb4eb08a56cc6383d62d0c68018a158324724
mdhamine <143548635+mdhamine@users.noreply.github.com>
2024-09-23 19:05:35 +0100

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <MainNavbar />
+    <MainHero class="py-16"/>
+    <MainPlans />
+    <MainContactus />
+    <MainSvgs/>
+    <MainFooter />
+    
+  </template>
+  
+  <script>
+ 
+  </script>
+  
