3c83fd6cbb82a5106771a604715073535a8fe1bb
mdhamine <143548635+mdhamine@users.noreply.github.com>
2024-09-29 22:17:52 +0100

    Update email.ts

--- server/api/email.ts
+++ server/api/email.ts
@@ -1,12 +1,13 @@
-
 import nodemailer from 'nodemailer';
+import { name } from '../../components/client1/info';
 
 
 export default eventHandler(async (event) => {
   try {
     const body = await readBody(event);
-    const { name, phone, state, cart, receiveremail,total , delivery } = body;
+    const { name, phone, state, cart, receiveremail,total , delivery, message, } = body;
     
+    console.log("cart cateory",cart)
     if (receiveremail !== "ee") {
         console.log("ahhhh");
     }
@@ -32,17 +33,19 @@ export default eventHandler(async (event) => {
                <li><strong>Name:</strong> ${name}</li>
                <li><strong>Phone:</strong> ${phone}</li>
                <li><strong>State:</strong> ${state}</li>
-               <li><strong>State:</strong> ${delivery}</li>
+               <li><strong>delivery:</strong> ${delivery}</li>
+               
                 <ul>
-                ${cart.map((item: { category: any; product: any; units: any;}) => `
+                ${cart.map((item: { category: any; product: any; units: any; categoryname: any;}) => `
                <li>
-                 <strong>Category:</strong> ${item.category}, 
+                 <strong>Category:</strong> ${item.categoryname}, 
                  <strong>Product:</strong> ${item.product.name}, 
                  <strong>Units:</strong> ${item.units}, 
                  
                </li>`).join('')}
                </ul>
-               <li><strong>total:</strong> ${total}</li>
+               <li><strong>message:</strong> ${message}</li>
+               <li><strong>total:</strong> ${total} DZD</li>
              </ul>`
     }; 
 
