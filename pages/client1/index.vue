cbf37c1d919701a6cd03181fd25d093e3b7477f1
mdhamine <143548635+mdhamine@users.noreply.github.com>
2024-09-29 11:29:12 +0100

    Create index.vue

---
+++ pages/client1/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col">
+      <Client1Nav />
+      <Client1Container/>
+      <Client1Footer class=""/>
+    </div>
+  </template>
+  
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
